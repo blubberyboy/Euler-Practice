9ab8b5cd2a6b0e0d7ad1d5ff33de1b6b233e4044
blubberyboy <ssaekoo@gmail.com>
2013-09-26 15:36:56 -0700

    Update counting_sundays.rb

--- counting_sundays.rb
+++ counting_sundays.rb
@@ -1,3 +1,15 @@
+#You are given the following information, but you may prefer to do some research for yourself.
+    
+#   1 Jan 1900 was a Monday.
+#    Thirty days has September,
+#    April, June and November.
+#    All the rest have thirty-one,
+#    Saving February alone,
+#    Which has twenty-eight, rain or shine.
+#    And on leap years, twenty-nine.
+#    A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+#    How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+    
 non_leap_year_months = [31,28,31,30,31,30,31,31,30,31,30,31]
 leap_year_months = [31,29,31,30,31,30,31,31,30,31,30,31]
 normal_year = 365

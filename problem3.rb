4694f43917e275b659639afa1f788ab307a1ed16
Stephen Saekoo <ssaekoo@gmail.com>
2013-09-28 11:10:11 -0700

    problem 1
    problem 1

---
+++ problem3.rb
@@ -0,0 +1,13 @@
+#The prime factors of 13195 are 5, 7, 13 and 29.
+
+#What is the largest prime factor of the number 600851475143 ?
+
+my_number = 600851475143
+
+for x = 600851475143
+
+	if 600851475143 % x == 0
+		
+		(0..x).each do |y|
+			
+			
